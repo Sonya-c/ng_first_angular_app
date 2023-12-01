1a146a4bb3651f29407668f63ffa935aa3427034
Sonya Castro <sonya.castro.gomez@gmail.com>
2023-11-30 22:49:06 -0500

    lesson 3

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     CommonModule,
+    HousingLocationComponent
   ],
   template: `
     <section>
@@ -13,6 +14,9 @@ import { CommonModule } from '@angular/common';
         <input type="text" placeholder="Filter by city">
         <button class="primary" type="button">Search</button>
       </form>
+    </section>
+    <section>
+      <app-housing-location></app-housing-location>
     </section>
   `,
   styleUrls: ['./home.component.css'],
